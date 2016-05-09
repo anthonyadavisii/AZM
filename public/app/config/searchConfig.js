3215e7a3fba05f37fda1817b65554ac503dbffab
Michael Hatmaker <Michael.Hatmaker@gmail.com>
2016-05-09 12:24:03 -0700

    Fixed last three curl download errors (but not image files yet)

--- public/app/config/searchConfig.js
+++ public/app/config/searchConfig.js
@@ -1,29 +1,71 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
-<title>404 - File or directory not found.</title>
-<style type="text/css">
-<!--
-body{margin:0;font-size:.7em;font-family:Verdana, Arial, Helvetica, sans-serif;background:#EEEEEE;}
-fieldset{padding:0 15px 10px 15px;} 
-h1{font-size:2.4em;margin:0;color:#FFF;}
-h2{font-size:1.7em;margin:0;color:#CC0000;} 
-h3{font-size:1.2em;margin:10px 0 0 0;color:#000000;} 
-#header{width:96%;margin:0 0 0 0;padding:6px 2% 6px 2%;font-family:"trebuchet MS", Verdana, sans-serif;color:#FFF;
-background-color:#555555;}
-#content{margin:0 0 0 2%;position:relative;}
-.content-container{background:#FFF;width:96%;margin-top:8px;padding:10px;position:relative;}
--->
-</style>
-</head>
-<body>
-<div id="header"><h1>Server Error</h1></div>
-<div id="content">
- <div class="content-container"><fieldset>
-  <h2>404 - File or directory not found.</h2>
-  <h3>The resource you are looking for might have been removed, had its name changed, or is temporarily unavailable.</h3>
- </fieldset></div>
-</div>
-</body>
-</html>
+﻿(function() {
+
+    "use strict";
+
+    define([
+        "esri/tasks/locator",
+        "esri/geometry/Extent",
+        "esri/layers/FeatureLayer"
+    ], function(Locator, Extent, FeatureLayer) {
+
+        var searchConfig = new function() {
+            var self = this;
+            // console.log(appConfig.layerInfo[1].queryUrl);
+
+            self.Sources = [{
+                locator: new Locator(appConfig.geoCoderService),
+                singleLineFieldName: "SingleLine",
+                autoNavigate: true,
+                enableInfoWindow: true,
+                enableHighlight: false,
+                autoSelect: false,
+                showInfoWindowOnSelect: true,
+                name: "Map",
+                searchExtent: new Extent({
+                    "xmin": -114.68,
+                    "ymin": 31.29,
+                    "xmax": -109.06,
+                    "ymax": 36.99
+                }),
+                placeholder: "302 N 1st Ave, Phoenix, Arizona"
+            }, {
+                featureLayer: new FeatureLayer(appConfig.layerInfo[1].queryUrl),
+                searchFields: ["NAME"],
+                displayField: "NAME",
+                outFields: ["*"],
+                name: "Counties",
+                placeholder: "Pinal County"
+            }, {
+                featureLayer: new FeatureLayer(appConfig.layerInfo[2].queryUrl),
+                searchFields: ["BdName"],
+                displayField: "BdName",
+                outFields: ["*"],
+                name: "Supervisorial Districts",
+                placeholder: "Supervisorial District 2"
+            }, {
+                featureLayer: new FeatureLayer(appConfig.placeService),
+                searchFields: ["NAME10"],
+                displayField: "NAME10",
+                outFields: ["*"],
+                name: "City/Town",
+                placeholder: "Scottsdale"
+            }, {
+                featureLayer: new FeatureLayer(appConfig.layerInfo[3].queryUrl),
+                searchFields: ["WARD"],
+                displayField: "WARD",
+                outFields: ["*"],
+                name: "Council Districts",
+                placeholder: "Glendale District 2 - Sahuaro"
+            }, {
+                featureLayer: new FeatureLayer(appConfig.censusTracts),
+                searchFields: ["NAMELSAD10"],
+                displayField: "NAMELSAD10",
+                outFields: ["*"],
+                autoNavigate: true,
+                name: "Census Tracts",
+                placeholder: "Census Tract 8.03"
+            }];
+        };
+        return searchConfig;
+    });
+}());
--- public/app/vm/alert2-vm.js
+++ public/app/vm/alert2-vm.js
@@ -1,29 +1,67 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
-<title>404 - File or directory not found.</title>
-<style type="text/css">
-<!--
-body{margin:0;font-size:.7em;font-family:Verdana, Arial, Helvetica, sans-serif;background:#EEEEEE;}
-fieldset{padding:0 15px 10px 15px;} 
-h1{font-size:2.4em;margin:0;color:#FFF;}
-h2{font-size:1.7em;margin:0;color:#CC0000;} 
-h3{font-size:1.2em;margin:10px 0 0 0;color:#000000;} 
-#header{width:96%;margin:0 0 0 0;padding:6px 2% 6px 2%;font-family:"trebuchet MS", Verdana, sans-serif;color:#FFF;
-background-color:#555555;}
-#content{margin:0 0 0 2%;position:relative;}
-.content-container{background:#FFF;width:96%;margin-top:8px;padding:10px;position:relative;}
--->
-</style>
-</head>
-<body>
-<div id="header"><h1>Server Error</h1></div>
-<div id="content">
- <div class="content-container"><fieldset>
-  <h2>404 - File or directory not found.</h2>
-  <h3>The resource you are looking for might have been removed, had its name changed, or is temporarily unavailable.</h3>
- </fieldset></div>
-</div>
-</body>
-</html>
+/**
+ * used to launch warning window
+ *
+ * @class warning-vm
+ */
+
+(function() {
+
+    "use strict";
+
+    define([
+            "dojo/dom-construct",
+            "dojo/topic",
+            "dojo/text!app/views/alert2-view.html"
+        ],
+        function(dc, tp, view) {
+
+            var alert2VM = new function() {
+
+                /**
+                 * Store reference to module this object.
+                 *
+                 * @property self
+                 * @type {*}
+                 */
+                var self = this;
+
+                /**
+                 * Initialize the class.
+                 *
+                 * @method init
+                 */
+                self.init = function() {
+                    // Place the HTML from the view into the main application after the map div.
+                    //dc.place(view, "map", "after");
+                    dc.place(view, "mapContainer", "after");
+
+                    $("#alert2Window").kendoWindow({
+                        width: "300px",
+                        height: "200px",
+                        title: "",
+                        actions: ["Close"],
+                        modal: true,
+                        visible: false,
+                        resizable: false
+                    }).data("kendoWindow");
+
+                }; // end init
+                //****************************************************************
+                /**
+                Method for opening the window.
+
+                @method openWindow
+                **/
+                self.openWindow = function() {
+                    var win = $("#alert2Window").data("kendoWindow");
+                    win.center();
+                    win.open();
+                };
+
+            }; // end AlertVM
+
+            return alert2VM;
+
+        } // end function
+    );
+}());
